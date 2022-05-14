5c4ef307c460a1a85e6eb42e037636170090df77
Ejikeme Dumaka <dumakaejikeme@gmail.com>
2022-05-15 00:04:03 +0100

    added a logic filter and search logic to payments

--- src/components/overview-payments/OverviewPayments.jsx
+++ src/components/overview-payments/OverviewPayments.jsx
@@ -4,8 +4,31 @@ import { images } from "../../constants/images";
 import Payments from "../../constants/payments";
 const OverviewPayments = () => {
   const [payments, setPayments] = useState(Payments);
+  const [query, setQuery] = useState("");
+  const [filterValue, setFilterValue] = useState("");
+  const sortOptions = ["All", "Reconcilled", "Un-reconcilled", "Pending"];
+  const onFormSearch = (e) => {
+    e.preventDefault();
+    if (query === "") {
+      setPayments(Payments);
+      return;
+    }
+    let filteredPayment = payments.filter((p) =>
+      p.title.toLowerCase().includes(query.toLowerCase())
+    );
+    setPayments(filteredPayment);
+  };
+  const filterTable = (e) => {
+    let value = e.target.value;
+    setFilterValue(value);
+    if (value === "All") {
+      setPayments(Payments);
+      return;
+    }
 
-  //   console.log(payments);
+    let filteredPayment = Payments.filter((p) => p.status === value);
+    setPayments(filteredPayment);
+  };
   return (
     <div className="row">
       <div className="col-12 mb-4">
@@ -28,24 +51,38 @@ const OverviewPayments = () => {
         </form>
       </div>
       <div className="col-12 col-md-3 mb-3">
-        <form className="d-flex align-items-center payment-search-form mb-4 mb-md-0">
+        <form
+          className="d-flex align-items-center payment-search-form mb-4 mb-md-0"
+          onKeyUp={onFormSearch}
+        >
           <span className="search-icon">
             <img src={images.searchIcon} alt="Search Icon" className="" />
           </span>
           <input
             className="form-control payment-search shadow-none"
             type="search"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
             placeholder="Search payments"
             aria-label="Search"
           />
         </form>
       </div>
-      <div className="col-12 col-md-3 mb-3">
+      <div className="col-12 col-md-4 col-xl-3 col-x mb-3">
         <form>
           <p className="d-flex align-items-center">
             <span className="select-text me-3"> Show </span>
-            <select className="form-control form-select shadow-none categories">
-              <option></option>
+            <select
+              className="form-control form-select shadow-none categories"
+              onChange={filterTable}
+              value={filterValue}
+            >
+              <option disabled>Select</option>
+              {sortOptions.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
             </select>
           </p>
         </form>
@@ -68,9 +105,9 @@ const OverviewPayments = () => {
               </tr>
             </thead>
 
-            <tbody>
-              {payments.length &&
-                payments.map((payment, index) => (
+            {/* {payments.length &&
+              payments.map((payment, index) => (
+                <tbody>
                   <tr key={`tr-${index}`}>
                     <td>{index + 1}</td>
                     <td>{payment.title}</td>
@@ -83,17 +120,37 @@ const OverviewPayments = () => {
                       </p>
                     </td>
                   </tr>
-                ))}
-            </tbody>
-            {/* <tfoot>
-              <tr scope="row " className="btn_row">
-                <td colspan={5}>
-                  <div className="no_data">
-                    <p className="no_data_text">No Record Found..</p>
-                  </div>
-                </td>
-              </tr>
-            </tfoot> */}
+                </tbody>
+              ))} */}
+
+            {payments.length === 0 ? (
+              <tfoot>
+                <tr className="btn_row">
+                  <td colSpan="5">
+                    <div className="no_data">
+                      <p className="no_data_text">No Record Found..</p>
+                    </div>
+                  </td>
+                </tr>
+              </tfoot>
+            ) : (
+              payments.map((payment, index) => (
+                <tbody key={index}>
+                  <tr key={`tr-${index}`}>
+                    <td>{index + 1}</td>
+                    <td>{payment.title}</td>
+                    <td>{`$${payment.price}`}</td>
+                    <td>{payment.transactionNumber}</td>
+                    <td>
+                      <p className={`status ${payment.status}`}>
+                        <span className="dot"></span>
+                        {payment.status}
+                      </p>
+                    </td>
+                  </tr>
+                </tbody>
+              ))
+            )}
           </table>
         </div>
       </div>
