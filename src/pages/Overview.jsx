608fe38ea47cb6ae5a6cf13b36d7a86411e8c9b1
Ejikeme Dumaka <dumakaejikeme@gmail.com>
2022-05-15 00:31:38 +0100

    added chart component in overview page

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,10 +1,12 @@
 import OverviewCards from "../components/overview-cards/OverviewCards";
+import OverviewCharts from "../components/overview-charts/OverviewCharts";
 import OverviewPayments from "../components/overview-payments/OverviewPayments";
 
 const Overview = () => {
   return (
     <>
       <OverviewCards />
+      <OverviewCharts />
       <OverviewPayments />
     </>
   );
